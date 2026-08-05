6e23d2879c56fc7af66b7c39d8e22be1679c0f75
LeoMMP <leonardommp08@gmail.com>
2026-08-05 14:20:14 -0300

    feat: aprimora interface, responsividade e adiciona currículo

--- script.js
+++ script.js
@@ -253,3 +253,37 @@ document.querySelectorAll(".fade-project").forEach(project => {
     projectObserver.observe(project);
 
 });
+
+// =============================
+// MENU ATIVO AO ROLAR
+// =============================
+
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("nav a");
+
+window.addEventListener("scroll", () => {
+
+    let current = "";
+
+    sections.forEach(section => {
+
+        const sectionTop = section.offsetTop - 120;
+        const sectionHeight = section.offsetHeight;
+
+        if (window.scrollY >= sectionTop) {
+            current = section.getAttribute("id");
+        }
+
+    });
+
+    navLinks.forEach(link => {
+
+        link.classList.remove("active");
+
+        if (link.getAttribute("href") === "#" + current) {
+            link.classList.add("active");
+        }
+
+    });
+
+});
